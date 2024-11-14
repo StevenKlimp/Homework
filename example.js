94321814f23ede141a9efc43a4a5d67eae872512
StevenKlimp <steven.klimp01@utrgv.edu>
2024-11-13 23:59:21 -0600

    Add files via upload

---
+++ example.js
@@ -0,0 +1,33 @@
+var listItems = document.getElementsByTagName('li');                   // All <li> elements
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var heading = document.querySelector('h2');                        // h2 element
+var headingText = heading.firstChild.nodeValue;                    // h2 text
+var totalItems = listItems.length;                                 // No. of <li> elements
+var newHeading =  headingText + '<span>' + totalItems + '</span>'; // Content
+
+
+/*  WRITE YOUR CODE BELOW */
+
+
+// Part1. Hint: Use the listItems object to iterate through the elements of the list
+for (var i = 0; i < listItems.length; i++) {
+    if (listItems[i].innerHTML === "honey") {
+        listItems[i].className = "cool";
+    } else if (listItems[i].innerHTML === "kale") {
+        listItems[i].className = "cool";
+    } else if (listItems[i].innerHTML === "fresh figs") {
+        listItems[i].className = "cool";
+    }
+}
+
+
+
+
+
+// Part2. Hint: Use the heading and newHeading to show the number of items in the shopping list
+heading.innerHTML = newHeading;
+
+
+
+
